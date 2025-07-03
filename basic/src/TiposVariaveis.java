e38829bb176d81c3381aea9427c740f9d13b8b2b
Gilson Almeida <gilson405719@hotmail.com>
2025-07-03 16:17:08 -0300

    add Tipos e Variaveis

---
+++ basic/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    byte idade = 123;
+    short ano = 2021;
+    int cep = 60442464;
+    long cpf = 12345678900L;
+    float pi = 3.14F;
+    double salario = 1275.33;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short)numeroNormal; //casting 
+    
+
+    final double VALOR_DE_PI = 3.141592653589; //Definição de Constante (Não pode mudar seu valor)
+}
