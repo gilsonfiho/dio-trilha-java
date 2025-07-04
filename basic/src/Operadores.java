ea87418ca89517763e7c0beecf462addb108334f
gilsonfiho <gilson405719@hotmail.com>
2025-07-03 22:35:56 -0300

    add project smart-tv

---
+++ basic/src/Operadores.java
@@ -0,0 +1,27 @@
+public class Operadores {
+    
+    public static void main(String[] args){
+        
+        // O operador "+" serve para soma de valores númericos e concatenação para Strings
+
+        //Num
+        int a = 1;
+        int b = 1;
+        int sum = a + b;
+        System.out.println(sum);
+
+        //String
+        String nome = "Gilson " +  "Alemeida";
+        System.out.println(nome);
+
+
+        // Operador Ternario
+        int c = 1;
+        int d = 2;
+        String resultado = c == d ? "True": "False";
+        System.out.println(resultado);
+
+    }
+
+
+}
